20b87f34071b851e8c2bee28433187c622006ebf
Amy <ctrlalternativedelete@gmail.com>
2021-02-05 19:09:52 +1100

    init

---
+++ form/process.php
@@ -0,0 +1,10 @@
+<?php
+$fName = $_POST['firstName'];
+$LName = $_POST['lastName'];
+$age = $_POST['age'];
+$gender = $_POST['gender'];
+$colour = $_POST['colour'];
+	
+echo "Hello $fName $LName, your age is $age and 
+your favoriate colour is $colour" ;  
+?>
