2810d702a34b96b9a62735fbe1ae25a3fe648ac7
Amy <ctrlalternativedelete@gmail.com>
2021-02-13 08:09:49 +1100

    updated styling on everything

--- form/process.php
+++ form/process.php
@@ -1,11 +1,22 @@
-<?php
-$fName = $_POST['firstName'];
-$LName = $_POST['lastName'];
-$age = $_POST['age'];
-$gender = $_POST['gender'];
-$colour = $_POST['colour'];
-get_defined_vars();
-echo "Hello $fName $LName, your age is $age and 
-your favoriate colour is $colour, $gender" ; 
+<!-- Written/Modified by June Yan (c) 2021 -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Customisation Form</title>
+        <meta name="description" content="June's Form Result">
+        <meta name="author" content="June Y">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+    </head>
 
-?>
+    <?php
+    $fName = $_POST['firstName'];
+    $LName = $_POST['lastName'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $colour = $_POST['colour'];
+    get_defined_vars();
+    echo "Hello $fName $LName, your age is $age and 
+    your favourite colour is $colour, Gender: $gender" ; 
+    ?>
+</html>
